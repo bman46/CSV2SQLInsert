92004ded92bf3f9e1e8a3f3740e9dfc969fbe632
Brendan McShane <brendanmcs7@gmail.com>
2022-04-19 22:34:11 -0400

    init

--- CSV2SQLInsert/Program.cs
+++ CSV2SQLInsert/Program.cs
@@ -1,3 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Globalization;
 
+Console.Title = "CSV to SQL Insert Tool";
+Console.WriteLine("CSV to SQL Insert Tool\n");
+string filePath;
+
+if (args.Length<1 || (filePath = string.Join(" ", args)) == "help" || (filePath = string.Join(" ", args)) == "-h")
+{
+    Console.WriteLine(
+        "CSV to SQL Insert Tool\n" +
+        "By: Brendan McShane (https://github.com/bman46)\n\n" +
+        "Usage:\n" +
+        "CSV2SQLInsert.exe (path to CSV file)\n" +
+        "Note that the output will include title row.\n\n" +
+        "Output Format:\n" +
+        "('title1','title2'),\n" +
+        "('cell1row1','cell2row1'),\n" +
+        "('cell1row2','cell2row2');"
+    );
+    return;
+}
+
+using (var reader = new StreamReader(filePath))
+{
+    string final = "";
+    string? line;
+    while ((line = reader.ReadLine()) != null)
+    {
+        final += "(";
+        String[] fields = line.Split(',');
+        foreach(string field in fields)
+        {
+            final += "'" + field + "',";
+        }
+        final=final.TrimEnd(',');
+        final += "),\n";
+    }
+    final=final.TrimEnd(',', '\n');
+    final += ";";
+    Console.WriteLine("Output:\n");
+    Console.WriteLine(final);
+}
